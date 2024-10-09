1a9edfab9b56e048fc3419dd0d3ee2260855955d
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2024-10-09 20:56:01 +0200

    removed build/

--- Patient_Data_Project/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Patient_Data_Project_FILE C:/Git_Repo/Informatik3/Patient_Data_Project/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/Patient_Data_Project.exe)
-set(__QT_DEPLOY_TARGET_Patient_Data_Project_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Patient_Data_Project_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
