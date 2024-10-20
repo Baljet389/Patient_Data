de3c61ded40e7ad640ee574ad2c4d387b9ef2920
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2024-10-20 20:21:11 +0200

    changed io_data class

--- Patient_Data_Project/io_data.cpp
+++ Patient_Data_Project/io_data.cpp
@@ -5,29 +5,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
+#include "io_data.h"
 
-class Einlesen
-{
-private:
-    int ID;
-    string vorname;
-    string nachname;
-    float geburt;
-    char geschlecht;
-    string adresse;
-    float tel_nummer;
-    string mail;
-    float datum;
-    string diagnose;
-    string behandlung;
-    string zeile;
 
-public:
 
-    // Einlesen();
-    // ifstream datei("C:/workspace/Studium/3. Semester/Informatik/patientendaten50000.csv");
+io_data::io_data(int ID,string vorname,string nachname,string geburt,string geschlecht,string adresse,string tel_nummer,string mail,string datum,string diagnose,string behandlung){
 
+}
+int io_data::returnAge(){
+    return 0;
+}
 
 
     // while(getline(datei,zeile))
@@ -36,4 +23,3 @@ public:
     //     zeilenpuffer >> nummer >> vorname >> nachname >> geburt >> geschlecht >> adresse >> telnummer >> mail >> datum >> diagnose >> behandlung;
     //     cout << nummer << "\n" << vorname << "\n" << nachname << "\n" << geburt << "\n" << geschlecht << "\n" << adresse << "\n" << telnummer << "\n" << mail << "\n" << datum << "\n" << diagnose << "\n" << behandlung << "\n";
     // }
-};
--- Patient_Data_Project/io_data.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef IO_DATA_H
-#define IO_DATA_H
-
-#endif // IO_DATA_H
