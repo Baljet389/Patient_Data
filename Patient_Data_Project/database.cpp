b33d18fbb3a12e082f545b9922b0bf212bf205d7
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2024-10-21 18:52:12 +0200

    implemented get Patient by ID method with prepared statements

--- Patient_Data_Project/database.cpp
+++ Patient_Data_Project/database.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string>
 #include <QString>
+#include "io_data.h"
+using namespace std;
 Database::Database() {
     db=QSqlDatabase::addDatabase("QSQLITE");
     std::string file=__FILE__;
@@ -35,3 +37,24 @@ void Database::createTable(){
         qDebug() << "Error: Could not create the database!" << db.lastError().text();
     }
 }
+io_data Database::getPatientbyID(int ID){
+    QSqlQuery query;
+    query.prepare("SELECT * FROM Patienten WHERE PatientID=?");
+    query.addBindValue(QString::number(ID));
+    bool successful=query.exec();
+
+    if(!successful){
+        qDebug() << "Error: Could not retrieve Patient with this ID!" << db.lastError().text();
+    }
+    query.first();
+    io_data data(ID,ToStdString(query.value(1)),ToStdString(query.value(2)),ToStdString(query.value(3)),
+                    ToStdString(query.value(4)),ToStdString(query.value(5)),ToStdString(query.value(6)),
+                    ToStdString(query.value(7)),ToStdString(query.value(8)),ToStdString(query.value(9)),
+                    ToStdString(query.value(10)));
+    return data;
+}
+string Database::ToStdString(const QVariant& value) {
+    return value.toString().toStdString();
+}
+
+
--- Patient_Data_Project/database.h
+++ Patient_Data_Project/database.h
@@ -4,13 +4,15 @@
 #include <QSqlQuery>
 #include <QSqlError>
 #include <QDebug>
-
+#include "io_data.h"
 class Database
 {
 public:
     QSqlDatabase db;
     Database();
     void createTable();
+    io_data getPatientbyID(int ID);
+    string  ToStdString(const QVariant& value);
 };
 
 #endif // DATABASE_H
