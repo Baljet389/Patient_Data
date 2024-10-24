8496252439dbc359815c046363b74940b526fa88
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2024-10-24 11:51:55 +0200

    Now can search for Data in any of the database columns

--- Patient_Data_Project/database.cpp
+++ Patient_Data_Project/database.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <string>
 #include <QString>
+#include <vector>
 #include "io_data.h"
+
 using namespace std;
 Database::Database() {
     db=QSqlDatabase::addDatabase("QSQLITE");
@@ -37,21 +39,28 @@ void Database::createTable(){
         qDebug() << "Error: Could not create the database!" << db.lastError().text();
     }
 }
-io_data Database::getPatientbyID(int ID){
+vector<io_data> Database::getPatientbyColumn(QString column,QString input){
+    vector<io_data> PatientList;
     QSqlQuery query;
-    query.prepare("SELECT * FROM Patienten WHERE PatientID=?");
-    query.addBindValue(QString::number(ID));
+    QString queryString = QString("SELECT PatientID, Vorname, Nachname, Geburtsdatum,"
+                                  " Geschlecht, Adresse, Telefonnummer, Email, Aufnahmedatum,"
+                                  " Diagnose, Behandlung FROM Patienten WHERE %1 = ?").arg(column);
+
+    query.prepare(queryString);
+    query.addBindValue(input);
     bool successful=query.exec();
 
     if(!successful){
-        qDebug() << "Error: Could not retrieve Patient with this ID!" << db.lastError().text();
+        throw "Column does not exist, or database connection is not available";
     }
-    query.first();
-    io_data data(ID,ToStdString(query.value(1)),ToStdString(query.value(2)),ToStdString(query.value(3)),
+    while(query.next()){
+    io_data data(query.value(0).toInt(),ToStdString(query.value(1)),ToStdString(query.value(2)),ToStdString(query.value(3)),
                     ToStdString(query.value(4)),ToStdString(query.value(5)),ToStdString(query.value(6)),
                     ToStdString(query.value(7)),ToStdString(query.value(8)),ToStdString(query.value(9)),
                     ToStdString(query.value(10)));
-    return data;
+    PatientList.push_back(data);
+    }
+    return PatientList;
 }
 string Database::ToStdString(const QVariant& value) {
     return value.toString().toStdString();
--- Patient_Data_Project/database.h
+++ Patient_Data_Project/database.h
@@ -1,17 +1,17 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 #include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QSqlError>
-#include <QDebug>
 #include "io_data.h"
+#include <vector>
+#include <QString>
+
 class Database
 {
 public:
     QSqlDatabase db;
     Database();
     void createTable();
-    io_data getPatientbyID(int ID);
+    std::vector<io_data> getPatientbyColumn(QString column,QString input);
     string  ToStdString(const QVariant& value);
 };
 
