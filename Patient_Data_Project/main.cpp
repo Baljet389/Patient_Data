73af24b99bd245d05f0e037b0b0393736c05f0d9
DESKTOP-NOP5UKF\jaege <jaeger6060@gmail.com>
2024-10-28 16:12:00 +0100

    removed test file

--- Patient_Data_Project/main.cpp
+++ Patient_Data_Project/main.cpp
@@ -1,17 +1,11 @@
 #include "mainwindow.h"
+
 #include <QApplication>
-#include "io_data.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    io_data Testobject(1, "Max", "Mustermann", "29.10.2005", "männlich", "Musterstraße 1", "0123456789", "max@example.com", "2023-10-28", "Diagnose A", "Behandlung A");
-    int abc=Testobject.returnAge();
-    int z;
     w.show();
-
-
-
     return a.exec();
 }
