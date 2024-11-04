5380722bcb76f7333ae7ac5e8315bfb811d09e86
J. Bauer-Chen <justin.bchen@gmail.com>
2024-11-04 17:43:57 +0100

    PateintDataDatabase.db has new temporary table: Users

--- Patient_Data_Project/user.h
+++ Patient_Data_Project/user.h
@@ -1,18 +1,22 @@
 #ifndef USER_H
 #define USER_H
-#include <string>
+//#include <string>         //Better use Qstring instead of string because of QT!
+#include <QString>
+
+//libs for Database
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 
 class user
 {
 public:
-    std::string name;
-    std::string pw_hashed;
-
-    user();
-    user(std::string name, std::string pw_hashed);
-    bool checkPW();
-    void encrpypt_hashing();
-    void decrypt_hashing();
+    int UID;                //User ID, not to be confused with patient/io_data ID
+    QString username;
+    // user();
+    user(int UID, QString name);
+    bool checkPW(QString pw_login);             //compares login PW-Hash with DB PW-Hash
+    private : QString encrypt_pw(QString password); //with Hashing
 };
 
 #endif // USER_H
