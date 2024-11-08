10e8000202b82458842fc8cf05f71794baa7ada5
AlexGvG <opportunity-alex@mail.de>
2024-11-08 21:26:15 +0100

    Zwischenspeichern

--- Patient_Data_Project/io_data.cpp
+++ Patient_Data_Project/io_data.cpp
@@ -71,3 +71,68 @@ int io_data::returnAge(){
     //     zeilenpuffer >> nummer >> vorname >> nachname >> geburt >> geschlecht >> adresse >> telnummer >> mail >> datum >> diagnose >> behandlung;
     //     cout << nummer << "\n" << vorname << "\n" << nachname << "\n" << geburt << "\n" << geschlecht << "\n" << adresse << "\n" << telnummer << "\n" << mail << "\n" << datum << "\n" << diagnose << "\n" << behandlung << "\n";
     // }
+
+
+#include <QDebug>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <QString>
+#include "io_data.h"
+#include "database.h"
+
+using namespace std;
+
+void io_data::CSVeinlesen(QString pfad) {
+    try {
+        qDebug() << "io_data::CSVeinlesen()";
+
+        ifstream datei(pfad.toStdString()); // Open file
+        if (!datei.is_open()) {
+            throw runtime_error("Could not open file");
+        }
+        qDebug() << "File opened:" << pfad;
+
+        string zeile;
+        while (getline(datei, zeile)) { // Read each line
+            qDebug() << "Read line:" << QString::fromStdString(zeile);
+
+            stringstream gelesene_zeile(zeile);
+            string wert;
+            vector<QString> werte;
+
+            // Parse the line into individual values separated by commas
+            while (getline(gelesene_zeile, wert, ',')) {
+                werte.push_back(QString::fromStdString(wert));
+                qDebug() << "Parsed value:" << QString::fromStdString(wert);
+            }
+
+            // Check if there are enough values before creating a `patient` object
+            if (werte.size() >= 11) {
+                io_data patient(
+                    stoi(werte[0].toStdString()), // ID
+                    werte[1],                     // First name
+                    werte[2],                     // Last name
+                    werte[3],                     // Date of birth
+                    werte[4],                     // Gender
+                    werte[5],                     // Address
+                    werte[6],                     // Phone number
+                    werte[7],                     // Email
+                    werte[8],                     // Date
+                    werte[9],                     // Diagnosis
+                    werte[10]                     // Treatment
+                    );
+
+                qDebug() << "Patient:" << patient.vorname << patient.nachname;
+
+                // Save the patient data to the database
+//                Database.addPatient(patient); // Assuming Database has an addPatient function
+            } else {
+                qDebug() << "Incomplete line, expected 11 values but got:" << werte.size();
+            }
+        }
+
+    } catch (const exception &e) {
+        qDebug() << "An error occurred:" << e.what();
+    }
+}
