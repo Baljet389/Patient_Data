6882ce5a16a9e8fd74ecc6ff4ec9eef9389e041b
AlexGvG <opportunity-alex@mail.de>
2024-11-23 23:06:27 +0100

    Mainwindow Daten speichern funktioniert auch über Menüleiste

--- Patient_Data_Project/main.cpp
+++ Patient_Data_Project/main.cpp
@@ -7,14 +7,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-
     Database database;
     LoginDialog l;
     if (l.exec() == QDialog::Accepted) {
     MainWindow w(nullptr,&database);
     w.showMaximized();
     w.show();
-
 
     return a.exec();
     }
