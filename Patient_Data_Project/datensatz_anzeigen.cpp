488a8bed7397c90ba2c849293745e03b86fb897e
AlexGvG <opportunity-alex@mail.de>
2024-12-29 13:29:47 +0100

    Datensatz anzeigen auf bearbeiten Anzahl an Fenster ausgeben

--- Patient_Data_Project/datensatz_anzeigen.cpp
+++ Patient_Data_Project/datensatz_anzeigen.cpp
@@ -51,9 +51,12 @@ void datensatz_anzeigen::on_pushButton_4_clicked()
         return;
     }
 
+    qDebug() << "TESTPOINT";
+    qDebug() << "Es sind " << mw->offeneFenster.size() << " bearbeiten/hinzufügen Fenster offen!";
+
     if (mw->offeneFenster.size() > 5)
     {
-        QMessageBox::warning(this, "Warnung", "Es sind bereits Fenster offen!");
+        QMessageBox::warning(this, "Warnung", "Es sind bereits " + QString::number(mw->offeneFenster.size()) + " Fenster offen!");
         return;
     }
 /*
